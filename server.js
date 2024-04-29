7af346f6d37c42196786f212a6b00af29cd2051e
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-04-29 10:33:19 +0100

    Modified server file and created layout

---
+++ server.js
@@ -0,0 +1,22 @@
+import express, { Router } from "express";
+import expressEjsLayouts from "express-ejs-layouts";
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+//body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("assets"));
+//port listerner
+app.listen(PORT, () => {
+  console.log(`Port is Listening on ${PORT}`);
+});
+//layout settings
+app.use(expressEjsLayouts);
+app.set("layout", "./layout/main");
+app.set("view engine", "ejs");
+//routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
